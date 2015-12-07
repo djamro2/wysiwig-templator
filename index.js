8076081a559200ed3b2eba71edd5d85d141d33bd
Daniel Jamrozik <djamro2@illinois.edu>
2015-12-07 12:15:37 -0600

    save article in db and static files

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 var express    = require('express');
+var mongoose   = require('mongoose');
 var bodyParser = require('body-parser');
 var app        = express();
 
@@ -7,18 +8,54 @@ var app        = express();
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
 app.use(bodyParser.json());
+app.use(express.static('public'));
+
+//return the home page
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/editor.html');
+});
+
+//database stuff with mongoose/mongodb
+
+mongoose.connect('mongodb://localhost/wysiwig');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+  // do something here
+});
+
+//basic schema for an article
+var wysiwigSchema = mongoose.Schema({
+    text: String,
+    title: String,
+    author: String
+});
+
+var Wysiwig = mongoose.model('Wysiwig', wysiwigSchema);
 
 //route that takes the submitted article
 app.post('/sendarticle', function (req, res) {
-    console.log(req);
-    console.log(req.body);
 
-    res.send('Recieved the article');
+    var wysiwig = new Wysiwig({text: req.body.text});
+
+    wysiwig.save(function(error, result){
+        if (!error){
+            res.json(result);
+        } else {
+            res.send(error);
+        }
+    });
+});
+
+app.get('/article/:title', function(req, res){
+    //return that article in a template
 });
 
 var server = app.listen(8000, function () {
-  var host = server.address().address;
+  var host = 'localhost';
   var port = server.address().port;
   console.log('Wysiwig editor listening at http://%s:%s', host, port);
 });
